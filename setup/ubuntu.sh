e9e4ad51567163d0fcf9d8052ae9bbffdccf4b88
yakobos <yakobos@proton.me>
2026-01-02 21:08:57 -0800

    be more detailed, also this script should be used specifically for cluster node or dev machine for cluster

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -1,7 +1,36 @@
 # curl
 # git
+
+
 # k3s
+curl -sfL https://get.k3s.io | sh -
+# gets rid of need to use sudo for kubectl
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+
+# install git
+# git PAT
+# create PAT
+# set GITHUB_USER
+# set GITHUB_TOKEN
+
 # flux cd
+curl -s https://fluxcd.io/install.sh | sudo bash
+flux --pre
+# add auth-token so https is used instaead of ssh
+flux bootstrap github --owner=$GITHUB_USER --repository=cluster --branch=main --path=./clusters/staging --personal --auth-token
+# requires PAT entered manually
+
+
+kubectl port-forward pod/linkding-ASSADFASDSAFASD -n linkding 8080:9090
+
+
+# kubens
+
+
+# setup first linkding user
+kubectl exec -it <linkding_pod_name> -- python manage.py createsuperuser --username=<USERNAME> --email=<EMAIL>
+
 # tree
 # kubectx
 # 
